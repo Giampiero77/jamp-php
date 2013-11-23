c61bc8d7f086276d7b5bbace29328f99a9a0dfa0
afulvio@gmail.com <afulvio@gmail.com@adb79830-4741-e661-ceee-1b52439ff1ae>
2013-11-23 13:55:24 +0000

    embed html5 video (beta)

---
+++ obj/html5video.obj.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('div.obj.php');
+class ClsObj_html5video extends ClsObj_div {
+
+	/**
+	* Construct
+	* @param string $id ID object
+	*/
+	public function __construct($id) {
+		parent::__construct($id);
+		$this->property["canvas"]	= array("value" => 'canvas', "inherit" => false, "html" => false);
+		$this->property["autoplay"]	= array("value" => null, "inherit" => false, "html" => false);
+		$this->property["poster"]	= array("value" => null, "inherit" => false, "html" => false);
+	}
+
+	/**
+	* Generate the code html
+	* @param string $tab Tabs
+	*/
+	public function codeHTML($tab = "")
+	{
+		$code = '<video '.$this->getProperty("html", true, false).
+			(!empty($this->property["autoplay"]["value"]) ? ' autoplay' : '').
+			(!empty($this->property["poster"]["value"]) ? ' poster="'.$this->property["poster"]["value"].'"' : '').
+			(!empty($this->property["width"]["value"]) ? ' width="'.$this->property["width"]["value"].'"' : '').
+			(!empty($this->property["height"]["value"]) ? ' height="'.$this->property["height"]["value"].'"' : '').
+			'></video>';
+
+		$code .= '<canvas id="'.$this->property["canvas"]["value"].'"'.
+			(!empty($this->property["width"]["value"]) ? ' width="'.$this->property["width"]["value"].'"' : '').
+			(!empty($this->property["height"]["value"]) ? ' height="'.$this->property["height"]["value"].'"' : '').
+			'></canvas>';
+		return $code;
+	}
+
+}
+?>
