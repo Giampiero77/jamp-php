26e4c04b4f5aad7006ed29961ccb0d87d0aa2c58
giampiero.ancilletta@poste.it <giampiero.ancilletta@poste.it@adb79830-4741-e661-ceee-1b52439ff1ae>
2013-12-31 09:58:39 +0000

    Object FORM: Add HTML5 Attributes and Events

--- obj/form.obj.php
+++ obj/form.obj.php
@@ -27,10 +27,32 @@ class ClsObj_form extends ClsObject {
 		$this->property["method"]			= array("value" => null, "inherit" => false, "html" => true);
 		$this->property["enctype"]			= array("value" => null, "inherit" => false, "html" => true);
 		$this->property["target"]			= array("value" => null, "inherit" => false, "html" => true);
-		$this->property["accept-charset"]= array("value" => null, "inherit" => false, "html" => true);
-		$this->property["onreset"]			= array("value" => null, "inherit" => false, "html" => true);
-		$this->property["onsubmit"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["accept-charset"]		= array("value" => null, "inherit" => false, "html" => true);
 		$this->property["name"]				= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["accesskey"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["style"]				= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["tabindex"]			= array("value" => null, "inherit" => false, "html" => true);
+				
+		// Not supported in HTML5
+		$this->property["onreset"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["onsubmit"]			= array("value" => null, "inherit" => false, "html" => true);
+		
+		// Attribute HTML 5
+		$this->property["contenteditable"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["contextmenu"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["draggable"]			= array("value" => null, "inherit" => false, "html" => true);		
+		$this->property["dropzone"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["hidden"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["spellcheck"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["translate"]			= array("value" => null, "inherit" => false, "html" => true);
+		
+		// Events HTML 5
+		$this->property["oncontextmenu"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["onfocus"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["onformchange"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["onforminput"]		= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["oninput"]			= array("value" => null, "inherit" => false, "html" => true);
+		$this->property["oninvalid"]			= array("value" => null, "inherit" => false, "html" => true);				
 	}
 
 	/**
